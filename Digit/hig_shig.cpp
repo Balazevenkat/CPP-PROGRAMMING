e668016bb279042d875097d6d27e3c0c145d3049
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-04 19:15:14 +0530

    Create hig_shig.cpp

---
+++ Digit/hig_shig.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+void find_hig_shig(int *arr)
+{
+    int i=0;
+    int hig=0,shig=0;
+    for(i=0;i<5;i++)
+    {
+        if(arr[i]>hig)
+        {
+            shig=hig;
+            hig=arr[i];
+        }
+        else if(arr[i]<hig && arr[i]>shig)
+        {
+            shig=arr[i];
+        }
+    }
+    
+    cout<<"high: "<<hig<<" "<<"shig: "<<shig<<endl;
+    
+    
+}
+
+int main()
+{
+    srand(time(nullptr));
+    int arr[5];
+    
+    for(int i=0;i<5;i++)
+    {
+        arr[i]=rand()%(10-1+1)+1;
+        for(int j=0;j<i;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                i--;
+                break;
+            }
+        }
+    }
+    
+    cout<<"Elements: ";
+    for(int i=0;i<5;i++)
+    cout<<arr[i]<<" ";
+    
+    
+    find_hig_shig(&arr[0]);
+    
+    return 0;
+}
