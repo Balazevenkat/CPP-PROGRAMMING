84e7d68b3c382b2b1f3eeb411e1d6dfc2ff2d437
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-10 11:58:49 +0530

    Create oper_decrement.cpp

---
+++ oper_decrement.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+
+
+ class  test 
+ {
+   int x;
+   public:
+   test(int a)
+   {
+       x=a;
+   }
+   void copy( test val)//inter->inter of (val) values x is 60
+   {
+       x=val.x;
+   }
+   void compare(test a)
+   {
+       if(a.x==x)
+       {
+           cout<<"eqaul"<<endl;
+           return;
+       }
+       cout<<"not eqaul"<<endl;
+   }
+    int operator==(test a)
+    {
+        if(a.x==x)
+        return 1;
+        else
+        return 0;
+    }
+    test operator-(test a)
+    {
+        test tem(0);
+        tem.x=a.x-x;
+        
+        return tem;
+    }
+    void operator++()
+    {
+        ++x;
+        cout<<"incremaentd"<<endl;
+    }
+    void operator--()
+    {
+        --x;
+    }
+     void show()
+     {
+         cout<<"(upadted value): "<<x<<endl;
+     }
+ };
+
+int main()
+{
+    test t1(10);
+    test t2(10);
+    
+    t1.compare(t2);
+    
+    test t3(20);
+    test t4(30);
+    
+    if(t3==t4)
+    {
+        cout<<"eqaul"<<endl; 
+    }
+    else
+     cout<<"not eqaul"<<endl;
+     
+     
+     test t5(59);
+     
+     ++t5;
+     
+     t5.show();
+     
+     cout<<"___________"<<endl;
+     
+     test t7(0);
+     
+     t7=t3-t4;
+     
+     t7.show();
+     
+     test t8(0);
+     t8.copy(t5);
+     --t8;
+     
+     cout<<endl;
+     cout<<"decrement ";
+     t8.show();
+     
+}
