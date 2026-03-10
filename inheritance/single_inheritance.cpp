7e02a19a0fd3a1a123160358670fdc7e976e2d43
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-10 14:39:53 +0530

    Update single_inheritance.cpp

--- inheritance/single_inheritance.cpp
+++ inheritance/single_inheritance.cpp
@@ -1,1 +1,51 @@
 
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+class par 
+{
+    public:
+    par()
+    {
+        cout<<"parent constcutor"<<endl;
+    }
+    ~par()
+    {
+        cout<<"parent descourtor"<<endl;
+    }
+};
+
+class child:protected par
+{
+   int x;
+   public:
+   child()
+   {
+       cout<<"child constructor.."<<endl;
+       cout<<"Enter the value: ";
+       cin>>x;
+   }
+   void show()
+   {
+       cout<<"value in (child)object"<<endl;
+       cout<<"Value: "<<x<<endl;
+   }
+   ~child()
+   {
+       cout<<"child descourtor"<<endl;
+   }
+};
+
+int main()
+{
+    child obj;
+    
+    obj.show();
+    
+}
+
+//first child descourtor
+
+
+
