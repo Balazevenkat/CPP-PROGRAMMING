3e804514d87611ea60a6d24524408cdfd919bcc5
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-15 12:43:02 +0530

    Create basic.cpp

---
+++ Exception/basic.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+
+    try
+    {
+        if(b==0)
+            throw b;
+
+        cout<<"Result: "<<a/b<<endl;
+    }
+    catch(int x)
+    {
+        cout<<"Error: Division by zero"<<endl;
+    }
+}
