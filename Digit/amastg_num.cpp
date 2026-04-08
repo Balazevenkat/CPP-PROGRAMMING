1c743db308fd3b9f1ec1b59a0e2ad5d29a64eda8
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-04-08 18:06:31 +0530

    Create amastg_num.cpp

---
+++ Digit/amastg_num.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=153;
+    int tem=n,sum=0,d;
+    while(tem)
+    {
+        d=tem%10;
+        sum+=d*d*d;
+        
+        tem/=10;
+    }
+    
+       if((sum==n))
+       cout<<"amstr"<<endl;
+       else
+       cout<<"not amst";
+}
